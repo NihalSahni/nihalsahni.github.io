5b405106a00c06a811665ea542ec2d1cedc049b5
NihalSahni <nihalrsahni@gmail.com>
2025-12-20 10:59:13 -0500

    Add files via upload

--- Practice.js
+++ Practice.js
@@ -10,6 +10,36 @@ function HSSelect() {
 
 MS.onclick = "MSSelect";
 HS.onclick = "HSSelect";
-const newparagraph = document.createElement("p");
-const open = document.getElementById("OpenEndedContainer");
-open.appendElement(newparagraph);
+function AddOEQuestion() {
+  var container = document.getElementById("QuestionContainer");
+  container.insertAdjacentHTML(
+    "beforeend",
+    `
+  <h2>Questions</h2>
+  <h2>Answers</h2>
+  <input>
+    `
+  );
+}
+
+function AddMCQuestion() {
+  var ranks = document.getElementById("ranks");
+  var subjects = document.getElementById("SubjectContainer");
+  var title = document.getElementById("Title");
+  ranks.remove();
+  subjects.remove();
+  title.innerHTML = "Neural Trials Online";
+  var container = document.getElementById("QuestionContainer");
+  container.insertAdjacentHTML(
+    "beforeend",
+    `
+  <h2>Questions</h2>
+  <h2>Answers</h2>
+  <input id=c1 type="checkbox" onclick="check()">W</input>
+  <input id=c2 type="checkbox" onclick="check()">X</input>
+  <input id=c3 type="checkbox" onclick="check()">Y</input>
+  <input id=c4 type="checkbox" onclick="check()">Z</input>
+  
+    `
+  );
+}
