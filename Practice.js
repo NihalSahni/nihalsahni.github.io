1bd595e000002d58de9b2d617633234e38d75b4c
NihalSahni <nihalrsahni@gmail.com>
2026-01-10 11:03:21 -0500

    Add files via upload

--- Practice.js
+++ Practice.js
@@ -1,5 +1,6 @@
 var MS = document.getElementById("MS");
 var HS = document.getElementById("HS");
+var submit = document.getElementById("Submit");
 var questions = "";
 function MSSelect() {
   question = "MS";
@@ -10,36 +11,41 @@ function HSSelect() {
 
 MS.onclick = "MSSelect";
 HS.onclick = "HSSelect";
-function AddOEQuestion() {
-  var container = document.getElementById("QuestionContainer");
-  container.insertAdjacentHTML(
-    "beforeend",
-    `
-  <h2>Questions</h2>
-  <h2>Answers</h2>
-  <input>
-    `
-  );
-}
 
 function AddMCQuestion() {
+  submit.innerHTML = "Next Phase";
+  var randomQuestion =
+    Object.keys(MSQuestions)[
+      Math.floor(Math.random() * Object.keys(MSQuestions).length)
+    ];
   var ranks = document.getElementById("ranks");
   var subjects = document.getElementById("SubjectContainer");
   var title = document.getElementById("Title");
-  ranks.remove();
-  subjects.remove();
-  title.innerHTML = "Neural Trials Online";
-  var container = document.getElementById("QuestionContainer");
-  container.insertAdjacentHTML(
-    "beforeend",
-    `
-  <h2>Questions</h2>
+  if (randomQuestion.indexOf("W)") >= 0) {
+    var container = document.getElementById("QuestionContainer");
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+  <h2>${randomQuestion}</h2>
   <h2>Answers</h2>
-  <input id=c1 type="checkbox" onclick="check()">W</input>
+  <input id=c1 type= "checkbox" onclick="check()">W</input>
   <input id=c2 type="checkbox" onclick="check()">X</input>
   <input id=c3 type="checkbox" onclick="check()">Y</input>
   <input id=c4 type="checkbox" onclick="check()">Z</input>
-  
-    `
-  );
+  `
+    );
+  } else {
+    var container = document.getElementById("QuestionContainer");
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+  <h2>${randomQuestion}</h2>
+  <input>
+  `
+    );
+  }
+
+  ranks.remove();
+  subjects.remove();
+  title.innerHTML = "Neural Trials Online";
 }
