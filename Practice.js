541b03586fff2e767161fa8a726bc8f8e7ec5fa5
NihalSahni <nihalrsahni@gmail.com>
2026-02-07 11:02:27 -0500

    Add files via upload

--- Practice.js
+++ Practice.js
@@ -1,18 +1,22 @@
 var MS = document.getElementById("Cadet");
 var HS = document.getElementById("Lieutenant");
 var Subjects = [];
-var submit = document.getElementById("Submit");
-var questions = "";
-var questionType = "";
+var Submit = document.getElementById("Submit");
+var Questions = "";
+var QuestionType = "";
 var MaxQuestion = 30;
 function MSSelect() {
-  questions = "MS";
+  Questions = "MS";
 }
 function HSSelect() {
-  questions = "HS";
+  Questions = "HS";
 }
 
 function AddMCQuestion() {
+  if (Questions == "" || Subjects.length == 0) {
+    return;
+  }
+
   //Question limiter
   if (MaxQuestion <= 0) {
     return;
@@ -20,7 +24,7 @@ function AddMCQuestion() {
     MaxQuestion -= 1;
   }
   //Adding the Questions
-  submit.innerHTML = "Next Phase";
+  Submit.innerHTML = "Next Phase";
   var randomSubjects = Subjects[Math.floor(Math.random() * Subjects.length)];
   var randomQuestion =
     Object.keys(randomSubjects)[
@@ -46,7 +50,7 @@ function AddMCQuestion() {
   <input id=c4 type="checkbox" onclick="check()">Z</input>
   `,
     );
-    questionType = "MC";
+    QuestionType = "MC";
   } else {
     container.insertAdjacentHTML(
       "beforeend",
@@ -55,7 +59,7 @@ function AddMCQuestion() {
   <input id=OpenInput>
   `,
     );
-    questionType = "OE";
+    QuestionType = "OE";
   }
 
   ranks.remove();
@@ -63,9 +67,9 @@ function AddMCQuestion() {
   title.innerHTML = "Neural Trials Online";
 }
 function CheckQuestion(question, subject) {
-  if (questionType == "MC") {
+  if (QuestionType == "MC") {
   }
-  if (questionType == "OE") {
+  if (QuestionType == "OE") {
     var input = document.getElementById("OpenInput");
     if (input.value.includes(subject[question])) {
       alert("Correct");
@@ -74,65 +78,79 @@ function CheckQuestion(question, subject) {
 }
 function UpdateQuestions() {
   var h3 = document.getElementById("AmountQuestions");
-  var slider = document.getElementById("QuestionSlider");
-  h3.innerHTML = slider.value + " questions";
+  var slider = document.getElementById("Questionslider");
+  h3.innerHTML = slider.value + " Questions";
   MaxQuestions = slider.value;
 }
-function addSubject(subject) {
-  if (questions == "MS") {
-    if (subject == "Math") {
+function addSubject(subject, checkbox) {
+  if (Questions == "MS") {
+    if (subject == "Math" && checkbox.checked == true) {
       Subjects.push(MSMath);
+    } else if (subject == "Math" && checkbox.checked == false) {
     }
-    if (subject == "Physics") {
+    if (subject == "Physics" && checkbox.checked == true) {
       Subjects.push(MSPhysics);
+    } else if (subject == "Physics" && checkbox.checked == false) {
     }
-    if (subject == "Chemistry") {
+    if (subject == "Chemistry" && checkbox.checked == true) {
       Subjects.push(MSChemistry);
+    } else if (subject == "Chemistry" && checkbox.checked == false) {
     }
-    if (subject == "Biology") {
+    if (subject == "Biology" && checkbox.checked == true) {
       Subjects.push(MSBiology);
+    } else if (subject == "Biology" && checkbox.checked == false) {
     }
-    if (subject == "EarthSpace") {
+    if (subject == "EarthSpace" && checkbox.checked == true) {
       Subjects.push(MSEarthSpace);
+    } else if (subject == "EarthSpace" && checkbox.checked == false) {
     }
-    if (subject == "Energy") {
+    if (subject == "Energy" && checkbox.checked == true) {
       Subjects.push(MSEnergy);
+    } else if (subject == "Energy" && checkbox.checked == false) {
     }
-    if (subject == "Everything") {
+    if (subject == "Everything" && checkbox.checked == true) {
       Subjects.push(MSMath);
       Subjects.push(MSPhysics);
       Subjects.push(MSChemistry);
       Subjects.push(MSBiology);
       Subjects.push(MSEarthSpace);
       Subjects.push(MSEnergy);
+    } else if (subject == "Everything" && checkbox.checked == false) {
     }
   }
-  if (questions == "HS") {
-    if (subject == "Math") {
+  if (Questions == "HS") {
+    if (subject == "Math" && checkbox.checked == true) {
       Subjects.push(HSMath);
+    } else if (subject == "Math" && checkbox.checked == false) {
     }
-    if (subject == "Physics") {
+    if (subject == "Physics" && checkbox.checked == true) {
       Subjects.push(HSPhysics);
+    } else if (subject == "Physics" && checkbox.checked == false) {
     }
-    if (subject == "Chemistry") {
+    if (subject == "Chemistry" && checkbox.checked == true) {
       Subjects.push(HSChemistry);
+    } else if (subject == "Chemistry" && checkbox.checked == false) {
     }
-    if (subject == "Biology") {
+    if (subject == "Biology" && checkbox.checked == true) {
       Subjects.push(HSBiology);
+    } else if (subject == "Biology" && checkbox.checked == false) {
     }
-    if (subject == "EarthSpace") {
+    if (subject == "EarthSpace" && checkbox.checked == true) {
       Subjects.push(HSEarthSpace);
+    } else if (subject == "EarthSpace" && checkbox.checked == false) {
     }
-    if (subject == "Energy") {
+    if (subject == "Energy" && checkbox.checked == true) {
       Subjects.push(HSEnergy);
+    } else if (subject == "Energy" && checkbox.checked == false) {
     }
-    if (subject == "Everything") {
+    if (subject == "Everything" && checkbox.checked == true) {
       Subjects.push(HSMath);
       Subjects.push(HSPhysics);
       Subjects.push(HSChemistry);
       Subjects.push(HSBiology);
       Subjects.push(HSEarthSpace);
       Subjects.push(HSEnergy);
+    } else if (subject == "Everything" && checkbox.checked == false) {
     }
   }
 }
